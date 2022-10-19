8013e6c93c58b36f5c87e1fe3594d40b0f4d9e1d
GabrielCalhabeu <78052432+GabrielCalhabeu@users.noreply.github.com>
2022-10-19 15:41:39 -0300

    a

--- src/module-info.java
+++ src/module-info.java
@@ -4,8 +4,10 @@ module plmswithjavafx {
 	
 	requires javafx.controls;
 	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
+    requires org.junit.jupiter.api;
+    requires rt;
+
+    opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
     opens modelo.entidades to javafx.graphics, javafx.fxml, javafx.base;
     opens modelo.servicos to javafx.graphics, javafx.fxml;
